362ced67dd80d6d95bf0d812801a5954bbc176a3
Jesus Arciniega <arciniega.chubuntu@gmail.com>
2017-11-10 12:40:53 -0700

    Logout

--- nav.php
+++ nav.php
@@ -9,7 +9,9 @@ setlocale(LC_ALL,”es_ES”);
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <!-- <li><a href="sass.html">Sass</a></li> -->
         <li><a href="#!"><?php echo strftime("%A %d de %B del %Y"); ?></a></li>
-        <!-- <li><a href="collapsible.html">Salir</a></li> -->
+        <li><form class="" action="logout.php" method="post">
+            <input type="submit" name="logout" value="Salir">
+        </form></li>
       </ul>
     </div>
   </nav>
