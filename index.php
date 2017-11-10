d4559e24dffb05a5ba051566deb9ecdd42e26dda
Jesus Arciniega <arciniega.chubuntu@gmail.com>
2017-11-10 13:30:49 -0700

    Iconos apple inicio
    Iconos para anclar al inicio en dispositivos ios

--- index.php
+++ index.php
@@ -9,6 +9,10 @@ if($_SESSION["user_id"] == 0){
 <head>
     <meta charset="utf-8">
     <title>Dineros | Administrador de finanzas</title>
+    <link rel="apple-touch-icon" sizes="57x57" href="apple-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="apple-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="apple-icon-114x114.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="apple-icon-144x144.png" />
     <!-- JQuery 3.X-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <!-- Compiled and minified CSS -->
