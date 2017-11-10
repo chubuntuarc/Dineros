94ef2b86edec50c3b8e3da28eff0cc6aedd2d1d4
Jesus Arciniega <arciniega.chubuntu@gmail.com>
2017-11-10 11:59:11 -0700

    Login

---
+++ login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Dineros | Administrador de finanzas</title>
+    <!-- JQuery 3.X-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+    <!-- Iconos -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Estilo del index -->
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body class="grey lighten-3">
+    <?php include("nav.php"); ?>
+
+    <div class="row">
+        <div class="container">
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="container">
+            <div class="col s12 m6 offset-m3">
+                <p>Ingresa tu usuario y contraseña</p>
+        <div class="card-panel">
+          <span>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+          </span>
+        </div>
+      </div>
+        </div>
+    </div>
+
+    </script>
+</body>
+</html>
